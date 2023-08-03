dd77cdf2545d6e014d0f82741f6ee67a5eeaab1d
Patryk <p.zawistowski190798@gmail.com>
2023-08-03 21:02:33 +0200

    Split left side into components

--- src/App.js
+++ src/App.js
@@ -1,51 +1,17 @@
 function App() {
 	return (
 		<div className="container-main">
-			<LeftSide />
+			<LeftSide>
+				<Background />
+				<CardFront />
+				<CardBack />
+			</LeftSide>
 			<RightSide />
 		</div>
 	);
 }
-function LeftSide() {
-	return (
-		<div className="container-left-side-main">
-			<div className="container-left-side">
-				<img
-					className="left-side-bg"
-					src="/images/bg-main-desktop.png"
-					alt="background"
-				/>
-			</div>
-			<div className="card-front-container">
-				<img
-					className="card card-front"
-					src="images/bg-card-front.png"
-					alt="card-front"
-				/>
-				<img
-					className="bank-logo bank-logo-main"
-					src="images/icons/bank-logo-1.png"
-					alt="bank logo main"
-				/>
-				<img
-					className="bank-logo bank-logo-second"
-					src="images/icons/bank-logo-2.png"
-					alt="second bank logo"
-				/>
-				<p className="card-info card-number">0000 0000 0000 0000</p>
-				<p className="card-info card-holder">Jhon Doe</p>
-				<p className="card-info card-date">11/09</p>
-			</div>
-			<div className="card-back-container">
-				<img
-					className="card card-back"
-					src="images/bg-card-back.png"
-					alt="card-front"
-				/>
-				<p className="card-info card-cvv">000</p>
-			</div>
-		</div>
-	);
+function LeftSide({ children }) {
+	return <div className="container-left-side-main">{children}</div>;
 }
 function RightSide() {
 	return (
@@ -142,6 +108,56 @@ function RightSide() {
 				</form>
 			</div>
 		</div>
+	);
+}
+function CardFront({ cardNumber, cardHolder, expMonth, expYear }) {
+	return (
+		<div className="card-front-container">
+			<img
+				className="card card-front"
+				src="images/bg-card-front.png"
+				alt="card-front"
+			/>
+			<img
+				className="bank-logo bank-logo-main"
+				src="images/icons/bank-logo-1.png"
+				alt="bank logo main"
+			/>
+			<img
+				className="bank-logo bank-logo-second"
+				src="images/icons/bank-logo-2.png"
+				alt="second bank logo"
+			/>
+			<p className="card-info card-number">{cardNumber}</p>
+			<p className="card-info card-holder">{cardHolder}</p>
+			<p className="card-info card-date">
+				{expMonth}/{expYear}
+			</p>
+		</div>
+	);
+}
+
+function CardBack({ cvvValue }) {
+	return (
+		<div>
+			<div className="card-back-container">
+				<img
+					className="card card-back"
+					src="images/bg-card-back.png"
+					alt="card-front"
+				/>
+				<p className="card-info card-cvv">{cvvValue}</p>
+			</div>
+		</div>
+	);
+}
+function Background() {
+	return (
+		<img
+			className="left-side-bg"
+			src="/images/bg-main-desktop.png"
+			alt="background"
+		/>
 	);
 }
 export default App;
